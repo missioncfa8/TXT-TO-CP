f3d43b0473243bf6bcddc20529e13e7f04a22efa
missioncfa8 <missioncfa8+github@gmail.com>
2025-11-15 13:02:18 +0530

    app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def hello_world():
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png">
-    <titleSudoR2spr Repository</title>
+    <title>SudoR2spr Repository</title>
 	<link rel="icon" type="image/x-icon" href="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png">
 
 </head>
@@ -20,18 +20,18 @@ def hello_world():
     <div class="container" style="bg-dark text-red text-center py-3 mt-5">
         <a href="https://github.com/nikhilsaini098" class="card">
             <p>
-              />▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄<br 
-	      />██░▄▄▄░█░▄▄▀█▄░▄██░▀██░█▄░▄██<br 
-              />██▄▄▄▀▀█░▀▀░██░███░█░█░██░███<br 
-	      />██░▀▀▀░█░██░█▀░▀██░██▄░█▀░▀██<br 
-              />▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀<br />
+              ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄<br>
+	      ██░▄▄▄░█░▄▄▀█▄░▄██░▀██░█▄░▄██<br>
+              ██▄▄▄▀▀█░▀▀░██░███░█░█░██░███<br>
+	      ██░▀▀▀░█░██░█▀░▀██░██▄░█▀░▀██<br>
+              ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀<br>
                                              <br>
 
                 <b>v2.0.0</b>
             </p>
         </a>
     </div>
-	<br></br><br></br><br></br>
+	<br><br><br>
 	<footer class="bg-dark text-white text-center py-3 mt-5">
 	<center><img loading="lazy" class="object-none object-center" src="https://tinypic.host/images/2025/04/28/IMG_20250428_085026_585.jpg" width="40" height="40">
         Powered By SAINI 
